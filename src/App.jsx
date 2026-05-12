42a9ec243ff4038fd6192a7839b5289330f27f19
underfr <loicklaurent28@gmail.com>
2026-05-12 16:01:53 +0200

    Feature: add Event feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import "./App.css";
 import Day from "./Day";
-import FormCalendar from "./FormCalendar"
+import FormCalendar from "./FormCalendar";
 
 const MONTH = [
   "Janvier",
@@ -24,46 +25,107 @@ const DAY = [
   "Jeudi",
   "Vendredi",
   "Samedi",
-  "Dimanche"
-]
+  "Dimanche",
+];
 
-const DAY_LETTER = DAY.map(day => day.charAt(0))
+const DAY_LETTER = DAY.map((day) => day.charAt(0));
+
+const EVENT_LIST = [];
+for (let i = 0; i <= 30; i++) {
+  EVENT_LIST.push({ confirmed: false });
+}
 
 function App() {
+  const [eventList, setEventList] = useState(EVENT_LIST);
 
-  function handleClick(e){
-    document.querySelectorAll(".selected").forEach(el => {
-      el.classList.remove("selected")
-    })
-    e.target.classList.add("selected")
+  function handleClick(event) {
+    const DIV_DAYS = document.querySelectorAll(".days div");
+    DIV_DAYS.forEach((day) =>
+      day != event.target ? day.classList.remove("selected") : null,
+    );
+    event.target.classList.toggle("selected");
   }
 
-  function handleSubmit(e){
-    e.preventDefault()
-    console.log(e.target.eventName.value)
-    console.log(e.target.eventHour.value)
-    console.log(e.target.eventAdress.value)
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (
+      e.target.eventAdress.value == "" ||
+      e.target.eventHour.value == "" ||
+      e.target.eventName.value == ""
+    ) {
+      alert("Veuillez remplir tout les champs");
+      return;
+    }
+
+    const DATE = [...document.querySelectorAll(".days div")];
+
+    let selected;
+    DATE.forEach((el, index) => {
+      if (el.classList.contains("selected")) {
+        selected = index;
+      }
+    });
+
+    if (selected == null) {
+      alert("Veuillez selectionner un jour");
+      return;
+    }
+
+    const NEW_EVENEMENT = {
+      nom: e.target[0].value,
+      heure: e.target[1].value,
+      lieu: e.target[2].value,
+      index: selected,
+      confirmed: true,
+    };
+
+    const TAB = [...eventList];
+    TAB[selected] = NEW_EVENEMENT;
+    setEventList(TAB);
   }
 
-  const ROW = []
+  const ROW = [];
 
-  for(let i=1; i<=31; i++){
-    ROW.push(<Day className="backgroundOrange" key={i} day={i} onClick={handleClick}/>)
+  for (let i = 1; i <= 31; i++) {
+    ROW.push(
+      <Day
+        className="backgroundOrange"
+        key={i}
+        day={i}
+        onClick={handleClick}
+      />,
+    );
   }
-
+console.log(eventList)
   return (
-    <>
-      <article className="card">
-        <Day className="month" key={MONTH[9]} day={MONTH[9]}/>
-      </article>
-      <section className="grid-7">
-        {DAY_LETTER.map((day, index) => <Day key={index} day={day} className="days"/>)}
-      </section>
-      <section className="grid-7 days">
-        {ROW}
-      </section>
-      <FormCalendar onSubmit={handleSubmit}/>
-    </>
+    <div className="App">
+      <div className="calendarApp">
+        <article className="card">
+          <Day className="month" key={MONTH[9]} day={MONTH[9]} />
+        </article>
+        <section className="grid-7">
+          {DAY_LETTER.map((day, index) => (
+            <Day key={index} day={day} className="days" />
+          ))}
+        </section>
+        <section className="grid-7 days">{ROW}</section>
+      </div>
+      <div className="formApp">
+        <FormCalendar onSubmit={handleSubmit} />
+      </div>
+      <div className="eventApp">
+        <h2>Evénements</h2>
+        {eventList
+          .filter((e) => e.confirmed)
+          .map((e, index) => (
+            <p key={index}>
+              {e.nom} - {e.lieu} : {e.index} {MONTH[9]} {e.heure}
+            </p>
+          ))}
+          
+      </div>
+    </div>
   );
 }
 
--- src/FormCalendar.jsx
+++ src/FormCalendar.jsx
@@ -1,14 +1,14 @@
 export default function FormCalendar({onSubmit}){
 
     return (
-        <>
+        <div className="formCalendar">
         <h2>Nouvel Evénement</h2>
-        <form action="" onSubmit={onSubmit}>
+        <form action="" onSubmit={onSubmit} className="form">
             <input type="text" name="eventName" placeholder="Nom événement"/>
             <input type="text" name="eventHour" placeholder="12H00"/>
             <input type="text" name="eventAdress" placeholder="Adresse"/>
             <input type="submit" value="Envoyer" />
         </form>
-        </>
+        </div>
     )
 }
